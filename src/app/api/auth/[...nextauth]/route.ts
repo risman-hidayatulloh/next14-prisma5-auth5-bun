b4353aee2bc7eabe40317fb52cd2f73b6be74e68
Risman MyDigilern <risman100@gmail.com>
2024-07-26 13:44:18 +0700

    add auth v5, login with google

---
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,6 @@
+import { handlers } from "@/lib/auth";
+
+export const { GET, POST } = handlers;
+
+console.log("GET", GET);
+console.log("POST", POST);
---
+++ src/app/login/page.tsx
@@ -0,0 +1,21 @@
+import { signIn } from "@/lib/auth";
+
+export default function Login() {
+  return (
+    <form
+      action={async () => {
+        "use server";
+        await signIn("google");
+      }}
+    >
+      <div className="flex h-screen justify-center items-center">
+        <button
+          type="submit"
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        >
+          Login with Google
+        </button>
+      </div>
+    </form>
+  );
+}
